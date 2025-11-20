9b8bcd885d8d243f4f8966f5b6e9a1d1c4d28a8c
Jan Zahradnik <za.jan@seznam.cz>
2025-11-20 15:54:47 +0100

    init commit

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Application } from "pixi.js";
+import { setEngine } from "./app/getEngine";
+import { LoadScreen } from "./app/screens/LoadScreen";
+import { MainScreen } from "./app/screens/main/MainScreen";
+import { userSettings } from "./app/utils/userSettings";
+import { CreationEngine } from "./engine/engine";
+
+declare global {
+  var __PIXI_APP__: Application;
+}
+
+/**
+ * Importing these modules will automatically register there plugins with the engine.
+ */
+import "@pixi/sound";
+// import "@esotericsoftware/spine-pixi-v8";
+
+// Create a new creation engine instance
+const engine = new CreationEngine();
+setEngine(engine);
+
+(async () => {
+  // Initialize the creation engine instance
+  await engine.init({
+    background: "#2a2a2a",
+    resizeOptions: { minWidth: 768, minHeight: 1024, letterbox: false },
+  });
+
+  // Initialize the user settings
+  userSettings.init();
+
+  globalThis.__PIXI_APP__ = engine;
+  // Show the load screen
+  await engine.navigation.showScreen(LoadScreen);
+  // Show the main screen once the load screen is dismissed
+  await engine.navigation.showScreen(MainScreen);
+})();
